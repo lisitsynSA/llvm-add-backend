b552e27584dd63ac43623bfbcfaefd66894fd814
Lisitsyn Sergey <fox9508@ya.ru>
2025-03-23 17:06:00 -0400

    [Sim] 20. Add SimAsmBackend

--- llvm/lib/Target/Sim/MCTargetDesc/SimMCTargetDesc.cpp
+++ llvm/lib/Target/Sim/MCTargetDesc/SimMCTargetDesc.cpp
@@ -78,4 +78,6 @@ extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeSimTargetMC() {
   TargetRegistry::RegisterMCInstPrinter(TheSimTarget, createSimMCInstPrinter);
   // Register the MC Code Emitter.
   TargetRegistry::RegisterMCCodeEmitter(TheSimTarget, createSimMCCodeEmitter);
+  // Register the asm backend.
+  TargetRegistry::RegisterMCAsmBackend(TheSimTarget, createSimAsmBackend);
 }
--- llvm/lib/Target/Sim/MCTargetDesc/SimMCTargetDesc.h
+++ llvm/lib/Target/Sim/MCTargetDesc/SimMCTargetDesc.h
@@ -5,8 +5,17 @@ namespace llvm {
 class MCCodeEmitter;
 class MCContext;
 class MCInstrInfo;
+class MCAsmBackend;
+class MCObjectTargetWriter;
+class MCRegisterInfo;
+class MCSubtargetInfo;
+class MCTargetOptions;
+class Target;
 
 MCCodeEmitter *createSimMCCodeEmitter(const MCInstrInfo &MCII, MCContext &Ctx);
+MCAsmBackend *createSimAsmBackend(const Target &T, const MCSubtargetInfo &STI,
+                                  const MCRegisterInfo &MRI,
+                                  const MCTargetOptions &Options);
 } // namespace llvm
 
 // Defines symbolic names for Sim registers.  This defines a mapping from
