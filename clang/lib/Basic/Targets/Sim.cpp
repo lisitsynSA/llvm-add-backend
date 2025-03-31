962ff175cdfd3a671deb6a3a256130b96d8773ac
Lisitsyn Sergey <fox9508@ya.ru>
2025-03-31 19:31:07 -0400

    [Sim] 25. Add Sim ClangBuiltin support (flush and putpixel)

--- clang/lib/Basic/Targets/Sim.cpp
+++ clang/lib/Basic/Targets/Sim.cpp
@@ -1,9 +1,16 @@
 #include "Sim.h"
 #include "clang/Basic/Builtins.h"
 #include "clang/Basic/MacroBuilder.h"
+#include "clang/Basic/TargetBuiltins.h"
 
 using namespace clang;
 using namespace clang::targets;
+
+static constexpr Builtin::Info BuiltinInfo[] = {
+#define BUILTIN(ID, TYPE, ATTRS)                                               \
+  {#ID, TYPE, ATTRS, nullptr, HeaderDesc::NO_HEADER, ALL_LANGUAGES},
+#include "clang/Basic/BuiltinsSim.def"
+};
 
 void SimTargetInfo::getTargetDefines(const LangOptions &Opts,
                                      MacroBuilder &Builder) const {
@@ -11,5 +18,6 @@ void SimTargetInfo::getTargetDefines(const LangOptions &Opts,
 }
 
 ArrayRef<Builtin::Info> SimTargetInfo::getTargetBuiltins() const {
-  return std::nullopt;
+  return llvm::ArrayRef(BuiltinInfo,
+                        clang::Sim::LastTSBuiltin - Builtin::FirstTSBuiltin);
 }
