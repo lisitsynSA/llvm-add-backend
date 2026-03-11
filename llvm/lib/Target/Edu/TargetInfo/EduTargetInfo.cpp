d8c3e915d58d9bbe4950d3f98da1278c06634af3
Lisitsyn Sergey <fox9508@ya.ru>
2026-03-11 09:21:29 -0400

    [Edu] 2. Register Edu Target

--- llvm/lib/Target/Edu/TargetInfo/EduTargetInfo.cpp
+++ llvm/lib/Target/Edu/TargetInfo/EduTargetInfo.cpp
@@ -1,8 +1,16 @@
+#include "EduTargetInfo.h"
 #include "Edu.h"
 #include "llvm/MC/TargetRegistry.h"
 
 using namespace llvm;
 
+Target &llvm::getTheEduTarget() {
+  static Target TheEduTarget;
+  return TheEduTarget;
+}
+
 extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeEduTargetInfo() {
   EDU_DUMP_RED
+  RegisterTarget<Triple::edu> X(getTheEduTarget(), "edu",
+                                "Educational target for LLVM course", "EDU");
 }
---
+++ llvm/lib/Target/Edu/TargetInfo/EduTargetInfo.h
@@ -0,0 +1,12 @@
+#ifndef LLVM_LIB_TARGET_EDU_TARGETINFO_EDUTARGETINFO_H
+#define LLVM_LIB_TARGET_EDU_TARGETINFO_EDUTARGETINFO_H
+
+namespace llvm {
+
+class Target;
+
+Target &getTheEduTarget();
+
+} // end namespace llvm
+
+#endif // LLVM_LIB_TARGET_EDU_TARGETINFO_EDUTARGETINFO_H
